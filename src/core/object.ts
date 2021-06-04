3ab987f2be61bd1cc319c437cb8584292a7fd5ce
Hazork <arthur.fiorette@gmail.com>
2021-06-04 12:38:47 -0300

    core: Added code

---
+++ src/core/object.ts
@@ -0,0 +1,23 @@
+import { Unpacked } from '../types';
+
+/**
+ * Returns a new object with the specified properties
+ */
+export function extract<T, K extends (keyof T)[]>(obj: T, properties: K): Pick<T, Unpacked<K>> {
+  const newObj = {} as T;
+  for (const prop of properties) {
+    newObj[prop] = obj[prop];
+  }
+  return newObj;
+}
+
+/**
+ * Returns a new object without the specified properties
+ */
+export function exclude<T, K extends (keyof T)[]>(obj: T, properties: K): Omit<T, Unpacked<K>> {
+  const newObj = obj;
+  for (const prop of properties) {
+    delete newObj[prop];
+  }
+  return newObj;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export const VERSION = '1.0.0';
+
+export * as core from './core';
